aefedc3a3cd1f442920ac53f283f7c83cfc62dae
Andrei <andrei2.barlea@live.uwe.ac.uk>
2021-02-21 17:18:59 +0000

    Andrei - Implemented new enemies, added menu return function

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -6,21 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour
 {
-   public Text pointsText;
-   
-   public void Setup(int score)
-   {
+    public Text pointsText;
+
+    public void Setup(int score)
+    {
         pointsText.text = score.ToString() + " POINT";
 
         if (score > 1 || score == 0)
         {
             pointsText.text += "S";
         }
-   }
+    }
 
     public void restartButton()
     {
         //Dependant on build index
         SceneManager.LoadScene(1);
     }
+
+    public void menuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
+    
